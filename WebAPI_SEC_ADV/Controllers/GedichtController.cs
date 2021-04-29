0f22075a677896809f52f19c046cf71acd03784b
Simon Bovy <57450110+SimonBovyPXL@users.noreply.github.com>
2021-04-29 17:22:49 +0200

    WebAPI security Advanced

---
+++ WebAPI_SEC_ADV/Controllers/GedichtController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_SEC_ADV.Controllers
+{
+    [Route("api/Gedicht")]
+    [ApiController]
+    public class GedichtController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<List<string>> GetGedicht()
+        {
+            return Ok( new List<string>()
+            {
+                "Doe gewoon eens gek",
+                "Spring uit de ban",
+                "Maak uitbundig plezier",
+                "En geniet ervan"
+            });
+        }
+    }
+}
---
+++ WebAPI_SEC_ADV/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Writers;
+
+
+namespace WebAPI_SEC_ADV.Controllers
+{
+    [ApiController]
+    [Route("api/Students")]
+    [Authorize]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        
+        public ActionResult<List<Student>> GetAll()
+        {
+            return Ok (new []
+            {
+                new Student{ Name = "Carolien Broux" },
+                new Student{ Name = "Maaike Bex"},
+                new Student{ Name = "Stef Brackez"},
+                new Student{ Name = "Simon Bovy"}
+            });
+        }
+    }
+}
